5314cdfb188dc30683b83c573d3f04f55626493f
sanjivanisharma <sanjivanisharma01@gmail.com>
2025-01-04 13:55:51 +0530

    Changed images

---
+++ script.js
@@ -0,0 +1,47 @@
+/* =================================== toggle Icon Navbar ==================================== */
+
+let menuIcon = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark')
+    navbar.classList.toggle('active')
+}
+
+/* ================================= Scroll section active link ================================ */
+
+let sections = document.querySelectorAll('.section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+
+    /* ================================= Sticky navbar ================================ */
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    /* ============================== Remove toggle icon and navbar ============================ */
+    menuIcon.classList.remove('fa-xmark')
+    navbar.classList.remove('active')
+}
+
+/* ================================= Typed JS ================================ */
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Developer', 'Web Designer', 'Coder'],
+    typeSpeed: 70,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop: true
+})
